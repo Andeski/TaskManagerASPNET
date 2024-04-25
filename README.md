974940c21e9a0f480daf32eec66d0b55298e89de
Andeski <42132710+Andeski@users.noreply.github.com>
2024-04-25 23:08:08 +0300

    asd

--- README.md
+++
@@ -1,1 +0,0 @@
-A Task Manager application that uses ASP.NET and mySQL for backend, and React + Typescript for frontend.
--- TaskManager.Server/obj/Debug/net8.0/TaskManager.Server.AssemblyInfo.cs
+++ TaskManager.Server/obj/Debug/net8.0/TaskManager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b872131090c11e8e6248240518ce5351872e2822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8581437537139ae40d4893853694649eb4bab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
